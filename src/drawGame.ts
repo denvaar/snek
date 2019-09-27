cd2d5abf929508c1cd1798a1ab040afeac91e95b
Matt Howa <matt.j.howa@gmail.com>
2019-09-27 15:30:29 -0600

    Added rainbow animation

--- src/drawGame.ts
+++ src/drawGame.ts
@@ -1,24 +1,40 @@
-import config from './config';
-import {pad} from './utils';
+import config from "./config";
+import { pad } from "./utils";
+
+let colorSpeed = 0;
+let colorModifier = 0;
 
 const colors = {
-  magenta: '\x1b[35m',
-  green: '\x1b[32m',
-  reset: '\x1b[0m',
+  red: "\x1b[31m",
+  yellow: "\x1b[33m",
+  green: "\x1b[32m",
+  blue: "\x1b[33m",
+  cyan: "\x1b[36m",
+  magenta: "\x1b[35m",
+  reset: "\x1b[0m"
 };
 
-const drawGame = ({snake, food, heading}: GameState): void => {
-  const {magenta, green, reset} = colors;
+const getRainbowColor = (index: number) => {
+  const { reset, ...rColors } = colors;
+  const rainbowColors = Object.values(rColors);
+
+  const moddedIndex = (index + colorModifier) % rainbowColors.length;
+
+  return rainbowColors[moddedIndex];
+};
+
+const drawGame = ({ snake, food, heading }: GameState): void => {
+  const { magenta, green, reset } = colors;
   const topRow = Array.from(
-    '┌────────────────────────────────────────────────────────────────────┐',
+      "┌────────────────────────────────────────────────────────────────────┐"
   );
   const bottomRow = Array.from(
-    '└────────────────────────────────────────────────────────────────────┘',
+      "└────────────────────────────────────────────────────────────────────┘"
   );
   const screen = [];
   for (let i = 0; i < config.screen.height; i++) {
     let rowData: Array<string> = Array.from(
-      '│                                                                    │',
+        "│                                                                    │"
     );
     if (i === 0) {
       rowData = topRow;
@@ -31,21 +47,27 @@ const drawGame = ({snake, food, heading}: GameState): void => {
     for (let s = 0; s < snake.length; s++) {
       const [col, row] = snake[s];
       if (row === i) {
-        rowData[col] = `${green}█${reset}`;
+        if (snake.length > 10) {
+          rowData[col] = `${getRainbowColor(s)}█${reset}`;
+        } else rowData[col] = `${green}█${reset}`;
       }
     }
 
-    screen.push(rowData.join(''));
+    colorSpeed++;
+    if(colorSpeed % 4 === 0)
+      colorModifier++;
+
+    screen.push(rowData.join(""));
   }
 
   const gameInfo = `┌────────────────────────────┐
 │  Snek Length: ${pad(snake.length, 2)}           │
 └────────────────────────────┘
 `;
-  const instructions = 'Move snek with arrow keys, or h,j,k,l';
+  const instructions = "Move snek with arrow keys, or h,j,k,l";
 
   console.clear();
-  console.log(`${screen.join('\n')}\n\n${gameInfo}\n${instructions}`);
+  console.log(`${screen.join("\n")}\n\n${gameInfo}\n${instructions}`);
 };
 
 export default drawGame;
