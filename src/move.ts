d7a118e010f47c9253584e7302d10ec04a824bdf
Daniel Król <daniel@krol.me>
2019-10-01 20:13:38 +0200

    Handle quick heading change
    Split processInputs into two parts
    Handle situation where user quickly turns around, if user managed
    to squeeze 2 keystrokes in single frame, it was possible to turn
    around in place and lose game

---
+++ src/move.ts
@@ -0,0 +1,40 @@
+const move = (state: GameState): GameState => {
+  switch (state.heading) {
+    case 'right':
+      return {
+        ...state,
+        snake: [
+          [state.snake[0][0] + 1, state.snake[0][1]],
+          ...state.snake.slice(0, -1),
+        ],
+      };
+    case 'left':
+      return {
+        ...state,
+        snake: [
+          [state.snake[0][0] - 1, state.snake[0][1]],
+          ...state.snake.slice(0, -1),
+        ],
+      };
+    case 'up':
+      return {
+        ...state,
+        snake: [
+          [state.snake[0][0], state.snake[0][1] - 1],
+          ...state.snake.slice(0, -1),
+        ],
+      };
+    case 'down':
+      return {
+        ...state,
+        snake: [
+          [state.snake[0][0], state.snake[0][1] + 1],
+          ...state.snake.slice(0, -1),
+        ],
+      };
+    default:
+      return state;
+  }
+};
+
+export default move;
