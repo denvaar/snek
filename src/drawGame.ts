024b0e5d5da9197b7d588ad6c38cf505b773360a
Denver Smith <denverpsmith@gmail.com>
2019-10-04 22:39:13 -0600

    console.clear does less flickering

--- src/drawGame.ts
+++ src/drawGame.ts
@@ -1,21 +1,21 @@
-import config from "./config";
-import { pad } from "./utils";
+import config from './config';
+import {pad} from './utils';
 
 let colorSpeed = 0;
 let colorModifier = 0;
 
 const colors = {
-  red: "\x1b[31m",
-  yellow: "\x1b[33m",
-  green: "\x1b[32m",
-  blue: "\x1b[33m",
-  cyan: "\x1b[36m",
-  magenta: "\x1b[35m",
-  reset: "\x1b[0m"
+  red: '\x1b[31m',
+  yellow: '\x1b[33m',
+  green: '\x1b[32m',
+  blue: '\x1b[33m',
+  cyan: '\x1b[36m',
+  magenta: '\x1b[35m',
+  reset: '\x1b[0m',
 };
 
 const getRainbowColor = (index: number) => {
-  const { reset, ...rColors } = colors;
+  const {reset, ...rColors} = colors;
   const rainbowColors = Object.values(rColors);
 
   const moddedIndex = (index + colorModifier) % rainbowColors.length;
@@ -23,18 +23,18 @@ const getRainbowColor = (index: number) => {
   return rainbowColors[moddedIndex];
 };
 
-const drawGame = ({ snake, food, heading }: GameState): void => {
-  const { magenta, green, reset } = colors;
+const drawGame = ({snake, food, heading}: GameState): void => {
+  const {magenta, green, reset} = colors;
   const topRow = Array.from(
-      "┌────────────────────────────────────────────────────────────────────┐"
+    '┌────────────────────────────────────────────────────────────────────┐',
   );
   const bottomRow = Array.from(
-      "└────────────────────────────────────────────────────────────────────┘"
+    '└────────────────────────────────────────────────────────────────────┘',
   );
   const screen = [];
   for (let i = 0; i < config.screen.height; i++) {
     let rowData: Array<string> = Array.from(
-        "│                                                                    │"
+      '│                                                                    │',
     );
     if (i === 0) {
       rowData = topRow;
@@ -54,20 +54,19 @@ const drawGame = ({ snake, food, heading }: GameState): void => {
     }
 
     colorSpeed++;
-    if(colorSpeed % 4 === 0)
-      colorModifier++;
+    if (colorSpeed % 4 === 0) colorModifier++;
 
-    screen.push(rowData.join(""));
+    screen.push(rowData.join(''));
   }
 
   const gameInfo = `┌────────────────────────────┐
 │  Snek Length: ${pad(snake.length, 2)}           │
 └────────────────────────────┘
 `;
-  const instructions = "Move snek with arrow keys, or h,j,k,l";
+  const instructions = 'Move snek with arrow keys, or h,j,k,l';
 
-  process.stdout.write('\x1Bc');
-  process.stdout.write(`${screen.join("\n")}\n\n${gameInfo}\n${instructions}`);
+  console.clear();
+  process.stdout.write(`${screen.join('\n')}\n\n${gameInfo}\n${instructions}`);
 };
 
 export default drawGame;
