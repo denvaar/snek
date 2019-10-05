1230f44ea79193acc52aa0ae16e2f53d60b1fdc8
evless <evless56@gmail.com>
2019-10-05 08:08:03 +0300

    feat: add prettier config and pre-commit/push hook

--- src/processPosition.ts
+++ src/processPosition.ts
@@ -1,37 +1,25 @@
 import config from './config';
-import {getRandomInt} from './utils';
+import { getRandomInt } from './utils';
 
-const growSnake = (
-  snake: Array<Array<number>>,
-  heading: Direction,
-): Array<Array<number>> => {
+const growSnake = (snake: Array<Array<number>>, heading: Direction): Array<Array<number>> => {
   if (heading === 'up' || heading === 'down') {
-    return [
-      ...snake,
-      [snake[snake.length - 1][0], snake[snake.length - 1][1] + 1],
-    ];
+    return [...snake, [snake[snake.length - 1][0], snake[snake.length - 1][1] + 1]];
   } else if (heading === 'left' || heading === 'right') {
-    return [
-      ...snake,
-      [snake[snake.length - 1][0] + 1, snake[snake.length - 1][1]],
-    ];
+    return [...snake, [snake[snake.length - 1][0] + 1, snake[snake.length - 1][1]]];
   }
 
   return snake;
 };
 
 const processPosition = (state: GameState): GameState => {
-  const {food, snake} = state;
+  const { food, snake } = state;
   const [snakeCol, snakeRow] = snake[0];
 
   if (food[1] === snake[0][1] && food[0] === snake[0][0]) {
     return {
       ...state,
       snake: growSnake(snake, state.heading),
-      food: [
-        getRandomInt(2, config.screen.width - 2),
-        getRandomInt(2, config.screen.height - 2),
-      ],
+      food: [getRandomInt(2, config.screen.width - 2), getRandomInt(2, config.screen.height - 2)],
     };
   }
 
@@ -53,7 +41,7 @@ const processPosition = (state: GameState): GameState => {
     snakeRow >= config.screen.height - 1 ||
     snakeRow <= 0
   ) {
-    return {...state, status: 'game_over'};
+    return { ...state, status: 'game_over' };
   }
 
   return state;
